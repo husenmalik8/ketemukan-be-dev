f342645d080fe2dd798c26f562fee4bc2f3bc03e
Husen <husenmalik7@gmail.com>
2025-07-09 09:59:58 +0700

    1

--- src/api/lost-comments/handler.js
+++ src/api/lost-comments/handler.js
@@ -1,55 +1,32 @@
-class LostsHandler {
+class LostCommentsHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
   }
 
-  postLostHandler = async (request, h) => {
-    this._validator.validateLostPayload(request.payload);
+  postLostCommentHandler = async (request, h) => {
+    this._validator.validateLostCommentPayload(request.payload);
 
-    const { title, shortDesc, description, lostDate } = request.payload;
+    const { content } = request.payload;
     const { id: userId } = request.auth.credentials;
+    const { id: lostId } = request.params;
 
-    const lostId = await this._service.addLost({
-      title,
-      shortDesc,
-      description,
-      lostDate,
+    const commentId = await this._service.addLostComment({
+      content,
+      lostId,
       userId,
     });
 
     const response = h.response({
       status: 'success',
-      message: 'Lost item berhasil ditambahkan',
+      message: 'Komentar berhasil ditambahkan',
       data: {
-        lostId,
+        commentId,
       },
     });
     response.code(201);
     return response;
   };
-
-  getLostsHandler = async () => {
-    const losts = await this._service.getLosts();
-    return {
-      status: 'success',
-      data: {
-        losts,
-      },
-    };
-  };
-
-  getLostByIdHandler = async (request) => {
-    const { id } = request.params;
-    const lostDetail = await this._service.getLostById(id);
-
-    return {
-      status: 'success',
-      data: {
-        lostDetail,
-      },
-    };
-  };
 }
 
-module.exports = LostsHandler;
+module.exports = LostCommentsHandler;
